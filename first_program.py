67fad42e0ec40104e6403a3a7d4c049f1dcab570
Muddassir Nazir <mnstalemate@hotmail.com>
2015-08-05 00:51:57 +0530

    Second Commit

--- first_program.py
+++ first_program.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
 #This is a comment and would be ignored.
 #Python Programming Practice
 #Author : Muddassir Nazir
 import math
-print "Hello"
+print ("Hello")
 print (math.sqrt(144))
 
+print (3+2)
 
