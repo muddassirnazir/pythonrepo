b9573731f45903a3c57b5f4a81c833532a2ab22d
Muddassir Nazir <mnstalemate@hotmail.com>
2015-08-30 01:10:12 +0530

    raw_input

--- first_program.py
+++ first_program.py
@@ -2,9 +2,18 @@
 #Python Programming Practice
 #Author : Muddassir Nazir
 
+#Printing a message
 print 'I am a python program'
+
+#Taking input from the keyboard
 name = raw_input("What is your name ?")
 print 'Hello..' + name
+
+#Storing data in a variable
+var = 2 + 2
+print var
+
+
 
 
 
