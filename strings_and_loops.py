9ecc179b1f107b9e8944b854be1fd1bb40cdf5ff
Muddassir Nazir <mnstalemate@hotmail.com>
2015-09-07 08:48:30 +0530

    loops

--- strings_and_loops.py
+++ strings_and_loops.py
@@ -1,23 +1,20 @@
 #Author: Muddassir Nazir
 
-#Strings and Loops
+#Strings & Loops
+
 month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 DaysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 for cntr in range(0,12):    print '%s has %d days' %(month[cntr],DaysOfMonth[cntr])
 
-st = 'He said, "Don\'t worry"'
+print 'Hello'
+print 'Muddassir\'s phone'
+print "He said, \"Don\'t worry\""
 
-#While loops
+string = "This is a string and I am gonna split it"
+print string.split(' ')
 
-x = 3
-
-while x != 0:
-	print(x)
-	x = x - 1
-	print("Wow, we've counted x down, and now it equals", x)
-	print "And now the loop has ended."
-
+#If-else statements
 if x == 10:
 	print "x is 10"
 elif x < 5:
@@ -25,15 +22,21 @@ elif x < 5:
 else:
 	print "x is something else"
 
-print 'Hello'
-print 'Muddassir\'s phone'
-print "He said, \"Don\'t worry\""
+#While loop-1
+x = 5
+while x != 0:
+	print(x)
+	x = x - 1
+	print("Wow, we've counted x down, and now it equals", x)
+	print "And now the loop has ended."
 
+#While loop-2
 count = 10
 while count >= 1:
 	count = count - 1
 	print count
 
+#While loop-3
 loop = 1
 while loop == 1:
 	response = raw_input("Write something or type 'quit' to exit =>")
@@ -42,6 +45,3 @@ while loop == 1:
 		loop = 0
 	else:
 		print 'You typed %s'	% response
-
-string = "This is a string and I am gonna split it"
-print string.split(' ')
