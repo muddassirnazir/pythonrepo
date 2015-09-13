d07258a801f1c54b488665e7a60365a7b5bfc68f
Muddassir Nazir <mnstalemate@hotmail.com>
2015-09-13 16:05:54 +0530

    random

---
+++ functions.py
@@ -0,0 +1,11 @@
+#Using random module
+import random
+for x in range(0,10):
+    print random.randint(0,55)
+
+#Specifically using the randint function from the random module
+from random import randint
+for cntr in range(0,14):
+    print randint(1,99)
+
+        
---
+++ simple_statements.py
@@ -0,0 +1,31 @@
+#Python Programming Practice
+#Author: Muddassir Nazir
+
+#This is a comment and would be ignored.
+#Printing a message
+print 'I am a python program'
+print "Don't do this"
+
+#Taking input from the keyboard
+name = raw_input("What is your name ?")
+print 'Hello..' + name
+
+#Storing data in a variable
+var = 2 + 2
+# print var
+
+#Strings
+string = '4'
+print string*4
+print int(string)*4
+
+var = 'A sentence'
+print var[2:5]
+
+#Simple for loop
+for counter in range(0,10):	print counter
+
+#Lists
+week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+print week
+print len(week)
