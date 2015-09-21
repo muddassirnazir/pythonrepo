12615493338e78f35a8ea5ab273213f68ce8b92e
Muddassir Nazir <mnstalemate@hotmail.com>
2015-09-21 23:11:57 +0530

    classes

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Dog():
+    def __init__(self,dogname,dogcolor,dogheight,dogbuild,dogmood,dogage):
+    #here we setup the attributes of our dog
+        self.name = dogname
+        self.color = dogcolor
+        self.height = dogheight
+        self.build = dogbuild
+        self.mood = dogmood
+        self.age = dogage
+        self.Hungry = False
+        self.Tired = False
