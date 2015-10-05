bf442858419e366fd03cfde8c2601d1e4b567a08
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-06 00:37:32 +0530

    random

--- math_builtins.py
+++ math_builtins.py
@@ -9,3 +9,17 @@ print float(23)
 print complex(3,2)
 #COnvert a number to binary
 print bin(245)
+print math.pi
+
+import random
+
+print random.choice(['red', 'blue', 'green'])
+
+# Simple Range 0 <= r < 6
+print random.randrange(6), random.randrange(6)
+# More complex range 1 <= r < 7
+print random.randrange(1,7), random.randrange(1,7)
+# Really complex range of even numbers between 2 and 36
+print random.randrange(2,37,2)
+# Odd numbers from 1 to 35
+print random.randrange(1,36,2)
