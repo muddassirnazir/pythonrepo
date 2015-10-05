9a736e08807e68ec8030be8c08961b89ccecff6e
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-05 23:52:25 +0530

    math_builtins

--- math_builtins.py
+++ math_builtins.py
@@ -1,1 +1,11 @@
 #Using builtin math functions
+import math
+
+#Raise x to power y
+print pow(4,3)
+#Print a number as a float value
+print float(23)
+#Print a complex number with 3 as real and 2 as imaginary parts
+print complex(3,2)
+#COnvert a number to binary
+print bin(245)
