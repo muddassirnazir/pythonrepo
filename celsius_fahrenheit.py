e6fabe368c910eaeb85ae8cf35b2972a0871887d
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-10 23:32:35 +0530

    fibonacci

--- celsius_fahrenheit.py
+++ celsius_fahrenheit.py
@@ -1,5 +1,14 @@
 #!/usr/bib/env python
+
 # Convert 8 C to F
 C=37.5
 F=32+C*(9./5.)
 print "celsius",C,"fahrenheit",F
+
+#Fibonacci Series
+a, b = 0, 1
+while b < 20:
+    print b
+    a, b = b, a + b
+
+    
