79c2ded1e7866749bbd7faea1b02c0c994596760
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-13 01:30:08 +0530

    break_continue.py

--- break_continue.py
+++ break_continue.py
@@ -7,3 +7,9 @@ for n in range (2,10):
             break
     else:
         print n, 'is a prime number'
+
+for num in range (2,10):
+    if num % 2 == 0:
+        print 'Found an even number', num
+        continue
+    print 'Found an odd number', num
