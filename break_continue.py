0d2456e627977d1268023852a81977380a542042
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-13 02:02:37 +0530

    break_continue.py

--- break_continue.py
+++ break_continue.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+'''
+The break statement in Python terminates the current loop and resumes execution at the next statement, just like the traditional break found in C.
+The most common use for break is when some external condition is triggered requiring a hasty exit from a loop. The break statement can be used in
+both while and for loops.
+'''
 for n in range (2,10):
     for x in range (2, n):
         if n % x == 0:
@@ -8,6 +13,10 @@ for n in range (2,10):
     else:
         print n, 'is a prime number'
 
+'''
+The continue statement in Python returns the control to the beginning of the while loop. The continue statement rejects all the remaining statements
+in the current iteration of the loop and moves the control back to the top of the loop. The continue statement can be used in both while and for loops.
+'''
 for num in range (2,10):
     if num % 2 == 0:
         print 'Found an even number', num
