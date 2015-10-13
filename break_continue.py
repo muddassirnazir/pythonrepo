31b0259cb6b18096a8c8f3d3b1f28dcf4e650fdd
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-14 01:22:39 +0530

    file_changes

--- break_continue.py
+++ break_continue.py
@@ -22,3 +22,15 @@ for num in range (2,10):
         print 'Found an even number', num
         continue
     print 'Found an odd number', num
+
+'''
+The pass statement in Python is used when a statement is required syntactically but you do not want any command or code to execute. The pass statement
+is a null operation; nothing happens when it executes. The pass is also useful in places where your code will eventually go, but has not been written
+yet (e.g., in stubs for example):
+'''
+for letter in 'Python':
+   if letter == 'h':
+      pass
+      print 'This is pass block'
+   print 'Current Letter :', letter
+print "Good bye!"
