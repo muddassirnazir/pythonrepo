75d13af16e41b0288f60f97dd109ff480fd517d8
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-17 23:50:39 +0530

    line_conversion

--- celsius_fahrenheit.py
+++ celsius_fahrenheit.py
@@ -10,3 +10,9 @@ a, b = 0, 1
 while b < 20:
     print b
     a, b = b, a + b
+
+# Compute line between two points.
+x1,y1 = 2,3 # point one
+x2,y2 = 6,8 # point two
+m,b = float(y1-y2)/(x1-x2), y1-float(y1-y2)/(x1-x2)*x1
+print "y=",m,"*x+",b
