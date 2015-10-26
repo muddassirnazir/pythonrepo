4f44979376756063d83756905f4ab9fea1df2d1c
Muddassir Nazir <mnstalemate@hotmail.com>
2015-10-27 01:58:59 +0530

    assert.py

--- assert.py
+++ assert.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python
 '''
-An assertion is a condition that we’re claiming should be true at this point in the program. Typically, it summarizes the state of the program’s
-variables. Assertions can help explain the relationships among variables, review what has happened so far in the program, and show that if statements
-and for or while loops have the desired effect. When a program is correct, all of the assertions are true no matter what inputs are provided. When a
-program has an error, at least one assertion winds up false for some combination of inputs. Python directly supports assertions through an assert
-statement. There are two forms: assert condition assert condition , expression If the condition is False, the program is in error;
-this statement raises an AssertionError exception. If the condition is True, the program is correct, this statement does nothing more.
+When a program is correct, all of the assertions are true no matter what inputs are provided. When a program has an error, at least one assertion winds
+up false for some combination of inputs. Python directly supports assertions through an assert statement.
+There are two forms:
+assert condition
+assert condition , expression
+'''
+max= 0
+a = raw_input('Write a number \n')
+b = raw_input('Write some another number \n')
+if a < b: max= b
+if b < a: max= a
+assert (max == a or max == b) and max >= a and max >= b
+
+'''
+Run this program with a equal to b and not equal to zero; it will raise the AssertionError exception. Clearly, the if statements don't set max to the
+largest of a and b when a = b. There is a problem in the if statements, and the presence of the problem is revealed by the assertion.
 '''
