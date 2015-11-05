6a2b75ae790a61f90f8a821f23f08facb102431c
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2015-11-05 18:03:02 +0530

    created window_gui.py

---
+++ window_gui.py
@@ -0,0 +1,15 @@
+import Tkinter
+
+window = Tkinter.Tk()
+window.title("Muddassir")
+window.geometry("300x300")
+#Adding labels and buttons
+label = Tkinter.Label(window, text="Label")
+entry = Tkinter.Entry(window)
+bttn = Tkinter.Button(window, text="Button")
+#Packing it in the window
+label.pack()
+entry.pack()
+bttn.pack()
+#Draw the window
+window.mainloop()
