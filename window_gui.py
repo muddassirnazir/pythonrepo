8baf5cc44be627b8d0b12ba37696995defc6811d
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2015-11-11 15:17:12 +0530

    wm_iconbitmap

--- window_gui.py
+++ window_gui.py
@@ -4,6 +4,8 @@ import Tkinter
 window = Tkinter.Tk()
 window.title("Muddassir")
 window.geometry("300x300")
+#We can set an icon in the window titlebut it must be bitmap
+#window.wm_iconbitmap('file.ico')
 #Adding labels and buttons
 label = Tkinter.Label(window, text="Label")
 entry = Tkinter.Entry(window)
