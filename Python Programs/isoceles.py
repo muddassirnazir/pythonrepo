61cd4410f94a019ecf1e0c74d45d0e8418a61637
Muddassir Nazir <mnstalemate@hotmail.com>
2015-11-25 02:17:16 +0530

    isoceles.py

---
+++ Python Programs/isoceles.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#Enter the side lenghts first
+#Logic : Two of the three sides are equal
+#Equilateral triangle is also be a isoceles
+print "First we have to enter sides of the triangle"
+a = raw_input("Enter the length of side a")
+b = raw_input("Enter the length of side b")
+c = raw_input("Enter the length of side c")
+a = int(a)
+b = int (b)
+c = int (c)
+#Write a program to check sides of a triangle and tell what type of triangle it is.
+if a == b or b == c or c == a:
+    print "The triangle is isoceles"
+else:
+    print "Not Isoceles"
+
+#Write a program to check if a triangle is right angled
+#Logic : The square of sum of two sides is greater than the square of third side.
+if ((a + b) ** 2) > (c ** 2) or ((b + c) ** 2) > (a ** 2) or ((c + a) ** 2) > (b ** 2):
+    print "We have a right angled triangle"
+else:
+    print "No right angled"
