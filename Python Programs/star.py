db32781f3f4d002d3c188fff57cc0b8b4a93da33
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2016-01-02 10:20:02 +0530

    line_shiftings

--- Python Programs/star.py
+++ Python Programs/star.py
@@ -9,15 +9,15 @@
 n = int(raw_input("Enter the number of lines"))
 for n in range (1, n):
     print "*" * (n)
-
-*****
+    
 '''
+*****
 ****
 ***
 **
 *
 '''
 n2 = int(raw_input("Enter the number of lines"))
-for n2 in range (n2, 0, -1):
 #range prints from n2 to 0 where -1 is the step.
+for n2 in range (n2, 0, -1): 
     print "*" * (n2)
