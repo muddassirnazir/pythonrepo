611b8f2aed802ee576a7ab549ecaec7b89cecad2
Muddassir Nazir <mnstalemate@hotmail.com>
2016-06-05 10:07:26 +0530

    star.py

--- Python Programs/star.py
+++ Python Programs/star.py
@@ -9,7 +9,7 @@
 n = int(raw_input("Enter the number of lines"))
 for n in range (1, n):
     print "*" * (n)
-    
+
 '''
 *****
 ****
@@ -19,5 +19,5 @@ for n in range (1, n):
 '''
 n2 = int(raw_input("Enter the number of lines"))
 #range prints from n2 to 0 where -1 is the step.
-for n2 in range (n2, 0, -1): 
+for n2 in range (n2, 0, -1):
     print "*" * (n2)
