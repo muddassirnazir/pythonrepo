e094f38c97313d51ee7c25fb23e4643c9ca47295
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2016-10-04 07:37:22 +0530

    change to python 3

--- Python Programs/fibonacci.py
+++ Python Programs/fibonacci.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #Program to print fibonacci series : The next number is sum of previous two numbers : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 
-print "Printing fibonacci series"
-nterm = int(raw_input("Enter the number of terms"))
+print ("Printing fibonacci series")
+nterm = int(input("Enter the number of terms"))
 a, b = 0, 1
 
 for i in range(nterm):
-    print a
+    print (a)
     a, b = b, a + b
