6910dc76ec144586227d39296404e91ef6ca1da6
Muddassir Nazir <muddassirnazir@users.noreply.github.com>
2016-10-06 03:11:36 +0530

    changed to python 3

--- Practice/celsius_fahrenheit.py
+++ Practice/celsius_fahrenheit.py
@@ -3,10 +3,10 @@
 # Convert 8 C to F
 C=37.5
 F=32+C*(9./5.)
-print "celsius",C,"fahrenheit",F
+print ("celsius",C,"fahrenheit",F)
 
 #Fibonacci Series
 a, b = 0, 1
 while b < 20:
-    print b
+    print (b)
     a, b = b, a + b
