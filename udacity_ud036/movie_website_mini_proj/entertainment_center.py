6b74353433c6d0f44bce8fa36c4f63ddab7c2e75
Muddassir Nazir <mnstalemate@hotmail.com>
2017-12-07 07:30:55 +0530

    more practice
    learned more on classes, predefined variable, object oriented concepts.

--- udacity_ud036/movie_website_mini_proj/entertainment_center.py
+++ udacity_ud036/movie_website_mini_proj/entertainment_center.py
@@ -5,5 +5,7 @@ avengers = media.Movie("The Avengers", "Superheroes unite to save the world", "h
 spectre = media.Movie("Spectre", "A spy on a mission to fight a crime syndicate", "https://upload.wikimedia.org/wikipedia/en/c/c3/Spectre_poster.jpg", "https://www.youtube.com/watch?v=z4UDNzXD3qA")
 godfather = media.Movie("The Godfather", "A story of power, family and revenge", "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg", "https://www.youtube.com/watch?v=sY1S34973zA")
 
-movies = [avengers, spectre, godfather]
-fresh_tomatoes.open_movies_page(movies)
+#movies = [avengers, spectre, godfather]
+#fresh_tomatoes.open_movies_page(movies)
+#print (media.Movies.VALID_RATINGS)
+#print (media.Movie.__doc__)
---
+++ udacity_ud036/predefined_class_variable.py
@@ -0,0 +1,7 @@
+class Test():
+    SOME_LIST = ["a", "b", "c"]
+
+#print (Test.__name__)
+#print (Test.__module__)
+#print (Test.__dict__)
+print (Test.__bases__)
