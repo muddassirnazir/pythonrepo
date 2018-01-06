be6894fc628035963cbe702fe403735104482728
Muddassir Nazir <mnstalemate@hotmail.com>
2018-01-07 03:40:20 +0530

    removed some files

--- Practice/stock.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# Compute the value of a block of stock
-
-shares = int( raw_input("shares: ") )
-price = float( raw_input("dollars: ") )
-price += float( raw_input("eights: ") )/8.0
-print "value", shares * price
