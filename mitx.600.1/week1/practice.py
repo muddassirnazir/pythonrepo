6aca804936de3b1dc046f4f3c4c572b6ba477a78
Muddassir Nazir <mnstalemate@hotmail.com>
2018-02-15 05:11:07 +0530

    moved to new repos

--- mitx.600.1/week1/practice.py
+++
@@ -1,4 +0,0 @@
-s = "abeddfgvsdcqwfsdcsvd"
-
-for i in range(len(s) - 1):
-    print(s[i + 1])
--- mitx.600.1/week1/problem_1.py
+++
@@ -1,7 +0,0 @@
-counter = 0
-
-for i in s:
-    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
-        counter += 1
-
-print('Number of vowels:', str(counter))
